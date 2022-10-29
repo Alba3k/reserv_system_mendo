ff139d6422d5e3b5f876ef6f4d22599f3bec51de
Alba3k <alexbyby@gmail.com>
2022-10-29 12:20:05 +0300

    Update settings.py

--- mendoclick/settings.py
+++ mendoclick/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@%s1l)rp_t-$37vt3iy440h*3b$ea$ta!h7#gk&-52)ihzqt22'
+SECRET_KEY = '*********************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = '***********************'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = '************************'
 
 # Application definition
 
